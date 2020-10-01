20bc7c24a879264ae9bf3b456b2cbc53b595ca13
Vanderlan Alves <vanderlan.comercial@outlook.com>
2020-09-30 21:17:41 -0300

    2020-09-30

---
+++ HelloWorld/SegundoSpring/semana/src/main/java/com/objetivos/semana/controller/ObjetivosController.java
@@ -0,0 +1,17 @@
+package com.objetivos.semana.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/objetivos")
+
+public class ObjetivosController {
+	@GetMapping
+	public String objetivos() {
+		return "Objetivos da semana: \nRevisar Java; \nRevisar MySQL; \nEntender bem SpringBoot.";
+	}
+	
+
+}
